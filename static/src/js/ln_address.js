04bde0ef8541fbafc803d3bfdd7a6091b42f15cf
Sagars209 <sagars209@gmail.com>
2023-06-17 18:13:29 +0530

    fix for multi employee pos

--- static/src/js/ln_address.js
+++ static/src/js/ln_address.js
@@ -7,11 +7,10 @@ odoo.define('custom_pos_receipt.models', function(require) {
     const CustomOrder = (Order) => class CustomOrder extends Order {
         export_for_printing() {
             var result = super.export_for_printing(...arguments);
-            result.employee_ln_address = this.pos.employee_ln_address;
-
-	    console.log('inside CustomOrder.export_for_printing');
-            console.log(this.pos.employee_ln_address);
-            result.employee_ln_address_qr=this.pos.employee_ln_address_qr_base64;
+            console.log('getting the cashier');
+            console.log(this.pos.get_cashier());
+            result.employee_ln_address = this.pos.get_cashier().employee_ln_address;
+            result.employee_ln_qr_image=this.pos.get_cashier().employee_ln_qr_image;
             return result;
         }
     }
